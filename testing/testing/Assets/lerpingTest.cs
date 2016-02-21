5738a1722fd1ff2e18fc6903c1e3cf62feb97fd9
TanWeiJie96 <syndicatepanda@hotmail.com>
2016-02-21 05:43:40 -0800

    Add Base Station

---
+++ testing/testing/Assets/lerpingTest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lerpingTest: MonoBehaviour {
+
+	//public Transform startMarker;
+	//public Transform endMarker;
+
+	public Vector3 initScale = new Vector3(1.0f, 1.0f, 1.0f);
+	public Vector3 scaleUpTo = new Vector3(1.5f, 1.5f, 1.5f);
+
+	public float speed = 1.0f;
+
+	public float startTime;
+	public float journeyLength;
+
+	void Start()
+	{
+		startTime = Time.time;
+		initScale = gameObject.transform.localScale;
+		journeyLength = Vector3.Distance(initScale, scaleUpTo);
+	}
+
+	void Update()
+	{
+		float distanceCovered = (Time.time - startTime) * speed;
+		float fracJourney = distanceCovered / journeyLength;
+		//	gameObject.transform.localScale = Vector3.Lerp (initScale, scaleUpTo, fracJourney);
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			gameObject.transform.localScale = Vector3.Lerp (initScale, scaleUpTo, fracJourney);
+			Debug.Log ("Pressed 'A'");
+		}
+
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			gameObject.transform.localScale = Vector3.Lerp (scaleUpTo, initScale, fracJourney);
+			Debug.Log ("Pressed 'A'");
+		}
+	}
+
+}
